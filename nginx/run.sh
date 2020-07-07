efec955bb9c5aab062dd8444266ef37c98d2b616
m4rk <marck20>
2020-07-07 02:02:30 +0200

    delete echo line

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 if [ ! "$(docker ps -aq -f name=nginx)" ]; then
-	echo "gonna run"
 	docker run --name nginx --hostname nginx --net=my-network -p 80:80 -d mynginx-image 
 else
 	if [ "$(docker ps -aq -f status=exited -f name=nginx)" ]; then
